66e0dbb6ddec456b5fb91c92f94b80e188447413
Joanmore <120502329+Joanmore@users.noreply.github.com>
2024-03-20 10:27:44 -0500

    Add files via upload

---
+++ JS/menu_l.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-toggle").addEventListener("click", function() {
+  document.getElementById("sidebar").classList.toggle("active");
+  document.getElementById("content").classList.toggle("active");
+});
